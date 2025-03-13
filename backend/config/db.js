723a425ffc888ebd0ab4ad9366334a6137b8be31
kalair22 <kalaivaniravi2208@gmail.com>
2025-03-13 14:25:00 +0530

    json

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI,{ dbName: "IC" }, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+  dbName: "IC",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
