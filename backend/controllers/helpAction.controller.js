e092a59ecceafe1937430c0bb944581ad15c5f69
kalair22 <kalaivaniravi2208@gmail.com>
2025-03-16 12:32:47 +0530

    sbt

--- backend/controllers/helpAction.controller.js
+++ backend/controllers/helpAction.controller.js
@@ -273,17 +273,17 @@ export const getUserStats = async (req, res) => {
 
 export const sbtmint = async (req, res) => {
   const { to } = req.body;
-  // const token = req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
-  // if (!token) {
-  //   return res.status(403).json({ message: "No token provided" });
-  // }
+  if (!token) {
+    return res.status(403).json({ message: "No token provided" });
+  }
 
-  // try {
-  //   jwt.verify(token, process.env.JWT_SECRET);
-  // } catch (error) {
-  //   return res.status(401).json({ message: "Invalid token" });
-  // }
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 
   if (!to) {
     return res.status(400).json({ message: "Wallet address is required" });
